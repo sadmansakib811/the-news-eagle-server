92a3d5ccf63d0acd09418a2a3d47333a4a784ae4
Sadman Sakib <sadmansakib123450@gmail.com>
2023-05-01 12:47:35 +0200

    rest api created from server

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const app = express();
 const cors = require('cors');
 app.use(cors())
 const port = process.env.PORT || 5000
-const categories = require('./data/categories.json')
+
+
+const categories = require('./data/categories.json');
+const news = require('./data/news.json')
+
+
+
 app.get('/', (req, res) => {
   res.send('Eagle is Flying')
 })
@@ -11,6 +17,31 @@ app.get('/', (req, res) => {
 app.get('/categories', (req, res) => {
     res.send(categories)
   })
+
+// getting news.json data:
+app.get('/news',(req, res)=>{
+  res.send(news)
+})
+
+app.get('/news/:id',(req, res)=>{
+  const id = req.params.id;
+  console.log(id);
+  const selectedNews = news.find(n=> n._id===id);
+  res.send(selectedNews)
+})
+
+app.get('/categories/:id',(req, res)=>{
+  const id = req.params.id;
+  console.log(id);
+  if(id == 0){
+    res.send(news);
+  }
+  else{
+    const selectedCategory = news.filter( n=> n.category_id === id );
+  res.send(selectedCategory);
+  }
+  
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
